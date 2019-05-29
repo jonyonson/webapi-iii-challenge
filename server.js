64beb7183a7eae355b6603a1220a83e8be7edf95
Jonathan Taylor <jonyonson@icloud.com>
2019-05-29 14:56:45 -0500

    logger middleware

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+// server.use(helmet());
+server.use(logger);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(`A ${req.method} request to '${req.url}'`);
+  next();
+}
 
 module.exports = server;
